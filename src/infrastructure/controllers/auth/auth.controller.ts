70cf78f2196b1edeae08483738d71aeb86629f2c
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2024-06-13 11:21:27 +0200

    watch flag add to NestJS CLI

--- src/infrastructure/controllers/auth/auth.controller.ts
+++ src/infrastructure/controllers/auth/auth.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Inject,
-  Post,
-  Req,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Inject, Post, Request } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiBody,
@@ -19,15 +10,10 @@ import {
 
 import { AuthLoginDto } from './auth-dto.class';
 import { IsAuthPresenter } from './auth.presenter';
-// import { JwtAuthGuard } from '../../common/guards/jwtAuth.guard';
-import { LoginGuard } from '../../common/guards/login.guard';
 // import JwtRefreshGuard from '../../common/guards/jwtRefresh.guard';
 import { UseCaseProxy } from 'src/infrastructure/usercases-proxy/usecases-proxy';
 import { UsecasesProxyModule } from 'src/infrastructure/usercases-proxy/usecases-proxy.module';
 import { LoginUseCases } from '../../../use-cases/auth/login.usecases';
-import { UserM } from 'src/domain/model/user';
-import { ApiResponseType } from '../../common/swagger/response.decorator';
-import { JwtAuthGuard } from 'src/infrastructure/common/guards/jwtAuth.guard';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -42,19 +28,6 @@ export class AuthController {
     @Inject(UsecasesProxyModule.LOGIN_USECASES_PROXY)
     private readonly loginUsecaseProxy: UseCaseProxy<LoginUseCases>,
   ) {}
-  // @Post('login')
-  // @ApiBearerAuth()
-  // @ApiBody({ type: AuthLoginDto })
-  // @ApiOperation({ description: 'login' })
-  // async login(
-  //   @Body() auth: AuthLoginDto,
-  // ): Promise<{ token: string; user: UserM }> {
-  //   const { email, password } = auth;
-  //   const { token, user } = await this.loginUsecaseProxy
-  //     .getInstance()
-  //     .execute(email, password);
-  //   return { token, user };
-  // }
   @Post('login')
   @ApiBearerAuth()
   @ApiBody({ type: AuthLoginDto })
@@ -72,16 +45,4 @@ export class AuthController {
     ]);
     return 'Login successful';
   }
-
-  // @Get('is_authenticated')
-  // @ApiBearerAuth()
-  // @UseGuards(JwtAuthGuard)
-  // @ApiOperation({ description: 'is_authenticated' })
-  // @ApiResponseType(IsAuthPresenter, false)
-  // async isAuthenticated(@Req() request: any) {
-  //   const user = await this.isAuthUsecaseProxy.getInstance().execute(request.user.username);
-  //   const response = new IsAuthPresenter();
-  //   response.username = user.username;
-  //   return response;
-  // }
 }
